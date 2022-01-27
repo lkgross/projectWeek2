e36f99397958057bf85710e78804322265b682b2
Gross <L1GROSS@bridgew.edu>
2022-01-27 15:16:01 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World!");
+        System.out.println(40 * 14.25);
+        // Inline initialization
+        double hoursWorked = 40;
+        System.out.println(hoursWorked / 3);
+        /*double hourlyWage; // Variable declaration
+        hourlyWage = 14.25; // Variable initialization
+        System.out.println(hoursWorked * hourlyWage);
+        var integerEntered = false;
+        System.out.println(8/2);
+        System.out.println(7/2);
+        System.out.println(7./2.);
+        System.out.println(7./2);
+        System.out.println(7/2.);
+        System.out.println(!integerEntered);
+        System.out.println(true && false);
+        System.out.println(true || false);
+        System.out.println();*/
+        String name = "Laura";
+        System.out.println(name);
+        char initial = 'L';
+        System.out.println(initial);
+    }
+}
